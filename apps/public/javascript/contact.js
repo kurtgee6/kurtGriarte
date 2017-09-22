71ceba2655540e878f1e26634f3e3b20238dcd42
kurtgee6 <kurtgee11@gmail.com>
2017-09-22 15:10:12 -0400

    contacts page done

--- apps/public/javascript/contact.js
+++ apps/public/javascript/contact.js
@@ -6,22 +6,7 @@ $("#email").click(function () {
     Materialize.toast('Email: kurtgee11@gmail.com ', 5000)
 });
 
-$("#github").click(function () {
-    Materialize.toast('Github: https://github.com/kurtgee6 ', 5000)
-});
-
-$("#linked").click(function () {
-    Materialize.toast('LinkedIn: https://https://www.linkedin.com/in/kurt-griarte-ab424b115/ ', 5000)
-});
 
 $("#instagram").click(function () {
     Materialize.toast('Instagram: kurtgriarte ', 5000)
-});
-
-$("#twitter").click(function () {
-    Materialize.toast('Twitter: https://twitter.com/GriarteKurt ', 5000)
-});
-
-$("#stackoverflow").click(function () {
-    Materialize.toast('StackOverflow: https://stackoverflow.com/users/7878649/kurt-griarte ', 5000)
 });
