fa634acf51280d518388c9c0607e660e87448ff1
kurtgee6 <kurtgee11@gmail.com>
2017-09-22 11:41:45 -0400

    removed data folder and apiRoutes.js

--- apps/routing/htmlRoutes.js
+++ apps/routing/htmlRoutes.js
@@ -7,5 +7,16 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname + '/../public/index.html'));
     });
 
+    app.get('/story', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/story.html'));
+    });
+
+    app.get('/contact', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/contact.html'));
+    });
+
+    app.get('/projects', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/project.html'));
+    });
 
 };
