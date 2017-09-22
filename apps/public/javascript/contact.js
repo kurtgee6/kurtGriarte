815c592cdecb4edd3033136beb765d29b366358c
kurtgee6 <kurtgee11@gmail.com>
2017-09-22 09:05:21 -0400

    website functuanality 80% finished

---
+++ apps/public/javascript/contact.js
@@ -0,0 +1,27 @@
+$("#phone").click(function () {
+    Materialize.toast('Cell Number: (919)-923-3586 ', 5000)
+});
+
+$("#email").click(function () {
+    Materialize.toast('Email: kurtgee11@gmail.com ', 5000)
+});
+
+$("#github").click(function () {
+    Materialize.toast('Github: https://github.com/kurtgee6 ', 5000)
+});
+
+$("#linked").click(function () {
+    Materialize.toast('LinkedIn: https://https://www.linkedin.com/in/kurt-griarte-ab424b115/ ', 5000)
+});
+
+$("#instagram").click(function () {
+    Materialize.toast('Instagram: kurtgriarte ', 5000)
+});
+
+$("#twitter").click(function () {
+    Materialize.toast('Twitter: https://twitter.com/GriarteKurt ', 5000)
+});
+
+$("#stackoverflow").click(function () {
+    Materialize.toast('StackOverflow: https://stackoverflow.com/users/7878649/kurt-griarte ', 5000)
+});
---
+++ apps/public/javascript/index.js
@@ -0,0 +1,3 @@
+$("#name").click(function () {
+    Materialize.toast('I am a toast!', 4000) // 4000 is the duration of the toast
+});
