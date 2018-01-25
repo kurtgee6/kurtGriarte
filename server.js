ce788a4591c80b08991ff3398bb67969bb9d63a4
kurtgee6 <kurtgee11@gmail.com>
2018-01-25 13:30:12 -0500

    added formspree validation and styling

--- server.js
+++ server.js
@@ -28,62 +28,66 @@ app.use(express.static('apps/public'));
 //     res.sendFile(path.join(__dirname + '/../public/index.html'));
 // });
 
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////// using formspree for email now ////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
 
 //gets the HTML file stored in htmlRoute.js so users sees display
-require('./apps/routing/htmlRoutes.js')(app);
+// require('./apps/routing/htmlRoutes.js')(app);
 
-app.post('/send', function(req, res) {
+// app.post('/send', function(req, res) {
 
-        var from = req.body.email;
+//         var from = req.body.email;
 
-        var output = `
-        <p>You have a new contact request</p>
-        <h3>Contact Details</h3>
-        <ul>  
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-        <li>Phone: ${req.body.phone}</li>
-        </ul>
-        <h3>Message</h3>
-        <p>${req.body.message}</p>
-    `;
+//         var output = `
+//         <p>You have a new contact request</p>
+//         <h3>Contact Details</h3>
+//         <ul>  
+//         <li>Name: ${req.body.name}</li>
+//         <li>Email: ${req.body.email}</li>
+//         <li>Phone: ${req.body.phone}</li>
+//         </ul>
+//         <h3>Message</h3>
+//         <p>${req.body.message}</p>
+//     `;
 
-    // create reusable transporter object using the default SMTP transport
-    var transporter = nodemailer.createTransport({
-        service: "Gmail",
-        port: 465,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: 'businessfankurt@gmail.com', // generated ethereal user
-            pass: 'passwordStolen'  // generated ethereal password
-        },
-        tls:{
-        rejectUnauthorized:false
-        }
-    });
+//     // create reusable transporter object using the default SMTP transport
+//     var transporter = nodemailer.createTransport({
+//         service: "Gmail",
+//         port: 465,
+//         secure: true, // true for 465, false for other ports
+//         auth: {
+//             user: 'businessfankurt@gmail.com', // generated ethereal user
+//             pass: ''  // generated ethereal password
+//         },
+//         tls:{
+//         rejectUnauthorized:false
+//         }
+//     });
 
-    // setup email data with unicode symbols
-    var mailOptions = {
-        from: '"Nodemailer Contact" < ' + from  + '>', // sender address
-        to: 'dummytestrest@gmail.com', // list of receivers
-        subject: 'New Contact Request', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output // html body
-    };
+//     // setup email data with unicode symbols
+//     var mailOptions = {
+//         from: '"Nodemailer Contact" < ' + from  + '>', // sender address
+//         to: 'dummytestrest@gmail.com', // list of receivers
+//         subject: 'New Contact Request', // Subject line
+//         text: 'Hello world?', // plain text body
+//         html: output // html body
+//     };
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
+//     // send mail with defined transport object
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return console.log(error);
+//         }
 
-        console.log('Message sent: %s', info.messageId);   
+//         console.log('Message sent: %s', info.messageId);   
 
-        return res.redirect('/thankYou');
+//         return res.redirect('/thankYou');
 
-    });
+//     });
 
-});
+// });
 
 //PORT listener
 app.listen(PORT, function () {
